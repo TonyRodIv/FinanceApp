8f8275d8021d4112d228420847ec29ad3cbeaac7
Antony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2025-11-06 20:51:14 -0300

    atualiza o estilo de fundo da página e adiciona IDs a elementos principais para manipulação no JavaScript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const financeMainCard = document.getElementById('financeMainCard');
+
+const payName = document.getElementById('payName');
+const paidValue = document.getElementById('paidValue');
+const missValue = document.getElementById('missValue');
